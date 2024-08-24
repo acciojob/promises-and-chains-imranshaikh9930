226df14d77fca58669918e9a803e2fb45573a3ae
imran <87297004+imranshaikh9930@users.noreply.github.com>
2024-08-24 16:08:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-//your JS code here. If required.
+const nameIn = document.getElementById("name");
+const ageIn = document.getElementById("age");
+const myForm = document.getElementById("myForm");
+const btn = document.getElementById("btn");
+
+
+function validate(name,age){
+    return name.trim() !== "" && !isNaN(age) && age.trim() !== "";
+}
+myForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const name = nameIn.value.trim();
+    const age = ageIn.value.trim();
+
+   const isValid =  validate(name,age);
+
+   function checkAge(){
+    return new Promise((resolve,reject)=>{
+
+        setTimeout(()=>{
+        if(isValid && ageIn.value > 18){
+                resolve(`Welcome, ${name}. You can vote.`)
+            }else{
+                reject(`Oh sorry ${name}. You aren't old enough`)
+            }
+        },4000)
+       })
+   }
+ 
+   checkAge().then((res)=>{
+        alert(res);
+   }).catch((err)=>alert(err));
+})
